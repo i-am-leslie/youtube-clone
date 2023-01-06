39b96219edb9039c18262ea112d59fc00d6fd958
leslie <leslieejeh@cmail.com>
2023-01-05 21:06:23 -0500

    youtube icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import {Stack} from '@mui/material'
-import  {Link}  from 'react-router-dom'
-import {logo} from '../assets/Constant'
+import { Link } from "react-router-dom";
+import {logo} from "../assets/Constant"
 import './Navbar.css'
 import {BsYoutube} from 'react-icons/bs'
 
 const Navbar = () => {
-  <Stack direction="row" alignItems="center" p={2} sx={{position:'sticky',background:'black',top:0,justifyContent:'space-between'}}>
-    <link  className='links'>
-      <img src={logo} alt="logo" height={45}/>
-      <h4 className='drag_quotes'>Click and drag quotes to the right for more <a href="#portfolio"><BsYoutube/></a></h4>
-    </link>
+  return(
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" className='links'>
+      <a className='youtubeicon'><BsYoutube/><p className='Yotube'>Youtube</p></a>
+    </Link>
 
 
   </Stack>
+  )
 }
 
 export default Navbar
